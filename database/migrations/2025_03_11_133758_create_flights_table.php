be7d92b17382ff1524a12aec5a6ca85a74e04f6c
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-03-11 10:39:56 -0400

    add flight migration

---
+++ database/migrations/2025_03_11_133758_create_flights_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Enums\FlightStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('status')->default(FlightStatusEnum::REQUESTED->value);
+            $table->dateTimeTz('departune_date');
+            $table->dateTimeTz('return_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
