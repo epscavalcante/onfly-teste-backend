8a00d5da4f2a07a93ec1344dcdbfca0c754e7180
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-03-13 16:02:22 -0400

    chore: extends base exception

--- app/Exceptions/CannotApproveFlightWasCancelledException.php
+++ app/Exceptions/CannotApproveFlightWasCancelledException.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 
-class CannotApproveFlightWasCancelledException extends Exception
+class CannotApproveFlightWasCancelledException extends CannotApproveFlightException
 {
     public function __construct()
     {
